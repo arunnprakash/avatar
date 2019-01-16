ba3c38529e94bed77465310463f3ea56e47e5eb0
ARUNPRAKASH <arunnprakash@gmail.com>
2019-01-16 22:18:44 +0530

    duplicates removed

--- avatar-admin/src/app/services/authorization/api.module.ts
+++ avatar-admin/src/app/services/authorization/api.module.ts
@@ -1,16 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { DistrictService } from './districtservice.generated';
-import { VillageService } from './villageservice.generated';
 import { LanguageService } from './languageservice.generated';
-import { StateService } from './stateservice.generated';
-import { TalukService } from './talukservice.generated';
 import { RoleService } from './roleservice.generated';
 import { DistrictService } from './districtservice.generated';
 import { CountryService } from './countryservice.generated';
 import { VillageService } from './villageservice.generated';
 import { UserService } from './userservice.generated';
 import { TalukService } from './talukservice.generated';
-import { CountryService } from './countryservice.generated';
 import { StateService } from './stateservice.generated';
 import { ServiceConfig } from './serviceconfig';
 
@@ -21,11 +16,9 @@ export class APIModule {
             ngModule: APIModule,
             providers: [
                 {provide: ServiceConfig, useValue: serviceConfig},
-                DistrictService,
                 VillageService,
+                DistrictService,
                 LanguageService,
-                StateService,
-                TalukService,
                 RoleService,
                 UserService,
                 TalukService,

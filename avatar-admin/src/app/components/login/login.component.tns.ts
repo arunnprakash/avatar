436c136f9090e25c026c7e24d25737574a3bfa2a
ARUNPRAKASH <arunnprakash@gmail.com>
2019-02-11 19:10:41 +0530

    get phone number for username

---
+++ avatar-admin/src/app/components/login/login.component.tns.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { device, screen, isAndroid, isIOS } from "tns-core-modules/platform";
+import { Telephony } from 'nativescript-telephony';
+
+import { UserService } from '../../services/authorization/userservice.generated';
+import { UserDTO } from "../../services/authorization/userdto.model";
+import { LoginRequest } from "../../services/authorization/loginrequest.model";
+import { LoginResponse } from "../../services/authorization/loginresponse.model";
+import { AuthService } from "../../services/auth.service";
+import { TranslateService } from '@ngx-translate/core';
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+    username: string;
+    password: string;
+    loginResponse: LoginResponse;
+    loginProgress: boolean;
+    loginError: boolean;
+  constructor(private userService: UserService, private authService: AuthService, 
+          private translate: TranslateService, private router: Router) { }
+
+  ngOnInit() {
+      Telephony().then((resolved) => {
+          console.log('resolved >', resolved)
+          console.dir(resolved);
+          this.username = resolved.phoneNumber;
+      }).catch((error) => {
+          console.error('error >', error)
+          console.dir(error);
+      });
+      this.password = 'admin';
+      if (this.authService.isLogged()) {
+          this.setPreferredLanguage(this.authService.getUserInfo().preferredLanguage.languageCode);
+          console.info("Already Logged in So Login Navigate to Home");
+          this.router.navigate(["/home"]).then( (e) => {
+              if (e) {
+                  console.log("Navigation to Home is successful!");
+                } else {
+                  console.log("Navigation to Home has failed!");
+                }
+          });
+      }
+  }
+  setPreferredLanguage(languageCode: string) {
+      // this language will be used as a fallback when a translation isn't found in the current language
+      this.translate.setDefaultLang('en');
+       // the lang to use, if the lang isn't available, it will use the current loader to get them
+      this.translate.use(languageCode);
+  }
+  doLogin() {
+      console.log("doLogin!");
+      let loginRequest: LoginRequest = new LoginRequest();
+      loginRequest.userNameOrMobileNumber = this.username;
+      loginRequest.password = this.password;
+      this.loginProgress = true;
+      this.loginError = false;
+      this.userService.login(loginRequest).subscribe(( loginResponse: LoginResponse ) => {
+          this.loginResponse = loginResponse;
+          this.loginProgress = false;
+          this.setPreferredLanguage(this.loginResponse.userDTO.preferredLanguage.languageCode);
+          this.authService.setToken(this.loginResponse.accessToken);
+          this.authService.setUserInfo(this.loginResponse.userDTO);
+          console.info("Login Navigate to Home");
+          this.router.navigate(["/home"]).then( (e) => {
+              if (e) {
+                  console.log("Navigation to Home is successful!");
+                } else {
+                  console.log("Navigation to Home has failed!");
+                }
+              });
+      },
+          ( error ) => {
+              this.loginError = true;
+              this.loginProgress = false;
+              console.error("userService.login error!");
+              console.error(error);
+          });
+  }
+
+  }

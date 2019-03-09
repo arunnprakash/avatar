05072ea7c7c13da768229801cf7851a2e3ab77c4
ARUNPRAKASH <arunnprakash@gmail.com>
2019-03-09 21:54:28 +0530

    date issue fixed

--- avatar-admin/src/app/services/transaction/sellerorder.model.ts
+++ avatar-admin/src/app/services/transaction/sellerorder.model.ts
@@ -3,5 +3,7 @@ import { SellerTransactionDTO } from './sellertransactiondto.model';
 
 export class SellerOrder {
     sellerTransaction: SellerTransactionDTO;
+    seller: { [index: string]: any };
+    sellerAgent: { [index: string]: any };
     priceTag: { [index: string]: any };
 }

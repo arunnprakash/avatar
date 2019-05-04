237708de4bc73032161010c4236faed77911d5fe
Arun <arun.prakash@mindgate.in>
2019-05-04 12:28:34 +0530

    updated for trucker driver endpoint

--- avatar-admin/src/app/services/authorization/api.module.ts
+++ avatar-admin/src/app/services/authorization/api.module.ts
@@ -1,19 +1,19 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { GenderService } from './genderservice.generated';
+import { QcWareHouseMappingService } from './qcwarehousemappingservice.generated';
+import { StateService } from './stateservice.generated';
+import { CountryService } from './countryservice.generated';
+import { LanguageService } from './languageservice.generated';
+import { WareHouseService } from './warehouseservice.generated';
 import { AssetService } from './assetservice.generated';
+import { TalukService } from './talukservice.generated';
 import { UserService } from './userservice.generated';
 import { RoleService } from './roleservice.generated';
-import { DistrictService } from './districtservice.generated';
-import { CountryService } from './countryservice.generated';
-import { TruckDriverWareHouseMappingService } from './truckdriverwarehousemappingservice.generated';
 import { VillageService } from './villageservice.generated';
-import { StateService } from './stateservice.generated';
-import { AssetTypeService } from './assettypeservice.generated';
-import { LanguageService } from './languageservice.generated';
+import { DistrictService } from './districtservice.generated';
+import { TruckDriverWareHouseMappingService } from './truckdriverwarehousemappingservice.generated';
 import { UserDeviceService } from './userdeviceservice.generated';
-import { QcWareHouseMappingService } from './qcwarehousemappingservice.generated';
-import { TalukService } from './talukservice.generated';
-import { WareHouseService } from './warehouseservice.generated';
-import { GenderService } from './genderservice.generated';
+import { AssetTypeService } from './assettypeservice.generated';
 import { ServiceConfig } from './serviceconfig';
 
 @NgModule({})
@@ -23,21 +23,21 @@ export class AuthorizationAPIModule {
             ngModule: AuthorizationAPIModule,
             providers: [
                 {provide: ServiceConfig, useValue: serviceConfig},
+                GenderService,
+                QcWareHouseMappingService,
+                StateService,
+                CountryService,
+                LanguageService,
+                WareHouseService,
                 AssetService,
+                TalukService,
                 UserService,
                 RoleService,
-                DistrictService,
-                CountryService,
-                TruckDriverWareHouseMappingService,
                 VillageService,
-                StateService,
-                AssetTypeService,
-                LanguageService,
+                DistrictService,
+                TruckDriverWareHouseMappingService,
                 UserDeviceService,
-                QcWareHouseMappingService,
-                TalukService,
-                WareHouseService,
-                GenderService
+                AssetTypeService
             ]
         };
     }
